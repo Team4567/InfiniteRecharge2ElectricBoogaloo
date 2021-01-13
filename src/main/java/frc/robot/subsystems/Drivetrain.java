1832ca88ed8b6e0e23ebf1612287c42825355ba4
AutoCars123 <partyitouch@icloud.com>
2021-01-12 22:59:50 -0500

    We're 2 days in and my commit messages are shit

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -19,6 +19,7 @@ import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.Constants;
@@ -40,47 +41,46 @@ public class Drivetrain extends SubsystemBase {
 		leftSlave = new WPI_TalonFX( Constants.kCANLSlave ), 
 		rightMaster = new WPI_TalonFX( Constants.kCANRMaster ), 
 		rightSlave = new WPI_TalonFX( Constants.kCANRSlave );
-	public DoubleSolenoid gearShift = new DoubleSolenoid( Constants.kCANPCMA, Constants.kPCMLGearboxIn, Constants.kPCMLGearboxOut );;
+  public DoubleSolenoid gearShiftL = new DoubleSolenoid( Constants.kCANPCMA, Constants.kPCMLGearboxIn, Constants.kPCMLGearboxOut );
+  public DoubleSolenoid gearShiftR = new DoubleSolenoid( Constants.kCANPCMA, Constants.kPCMRGearboxIn, Constants.kPCMRGearboxOut );;
 	public Gear gear, prevGear;
 	// The motors on the left side of the drive.
 
 
-  	private final SpeedControllerGroup m_leftMotors =
-    	new SpeedControllerGroup(leftMaster,
-                                 leftSlave);
+  	private final SpeedControllerGroup m_leftMotors = new SpeedControllerGroup( leftMaster, leftSlave );
 
   // The motors on the right side of the drive.
-  private final SpeedControllerGroup m_rightMotors =
-  	  new SpeedControllerGroup(rightMaster,
-  							   rightSlave);
+  private final SpeedControllerGroup m_rightMotors = new SpeedControllerGroup( rightMaster, rightSlave );
 
-  // The robot's drive
+  // The robot's drive, gives access to simplified drive commands. AUTOMATICALLY INVERTS ONE OF THE SIDES
   private final DifferentialDrive m_drive = new DifferentialDrive( m_leftMotors, m_rightMotors );
 
 
 				  
   
-  // The gyro sensor
+  // The gyro sensor, gives robots angle, acceleration, and turn rate.
   private final IMU m_gyro = new IMU( Constants.kCANIMU );
 
   // Odometry class for tracking robot pose
+  // Odometry simply means tracking a robot using a variety of sensors to apporximate its real life position
+  // We use odometry to follow paths to make sure we get to the right coordinates.
   private final DifferentialDriveOdometry m_odometry;
 
   /**
    * Creates a new DriveSubsystem.
    */
   public Drivetrain() {
-    // Sets the distance per pulse for the encoders
-    //m_leftEncoder.setDistancePerPulse( Constants.kEncoderDistancePerPulse );
-	//m_rightEncoder.setDistancePerPulse( Constants.kEncoderDistancePerPulse );
-	leftMaster.configSelectedFeedbackSensor( TalonFXFeedbackDevice.IntegratedSensor, 0, 0 );
-	// 2048 CPR
-	// 512 PPR (Motor Shaft)
-	// 7843.84 PPR (Output Shaft)
-	// 0.00012748857 RPP (Output Shaft)
-	// 0.00240310306 Inches Per Pulse (Wheel)
-	leftMaster.configSelectedFeedbackCoefficient( ( 6 * Math.PI ) / ( 2048 * lowGearRatio * 39.37 ) );
-	rightMaster.configSelectedFeedbackCoefficient( ( 6 * Math.PI ) / ( 2048 * lowGearRatio * 39.37 ) );
+    // Tell the leftMaster to reference its integrated encoder as its sensor
+    leftMaster.configSelectedFeedbackSensor( TalonFXFeedbackDevice.IntegratedSensor, 0, 0 );
+    // Tell the rightMaster to reference its integrated encoder as its sensor
+    rightMaster.configSelectedFeedbackSensor( TalonFXFeedbackDevice.IntegratedSensor, 0, 0 );
+	  // 2048 CPR
+	  // 512 PPR (Motor Shaft)
+	  // 7843.84 PPR (Output Shaft)
+	  // 0.00012748857 RPP (Output Shaft)
+	  // 0.00240310306 Inches Per Pulse (Wheel)
+	  leftMaster.configSelectedFeedbackCoefficient( ( 6 * Math.PI ) / ( 2048 * lowGearRatio ) );
+	  rightMaster.configSelectedFeedbackCoefficient( ( 6 * Math.PI ) / ( 2048 * lowGearRatio ) );
     resetEncoders();
     m_odometry = new DifferentialDriveOdometry( m_gyro.getRotation2d() );
   }
@@ -88,8 +88,10 @@ public class Drivetrain extends SubsystemBase {
   @Override
   public void periodic() {
     // Update the odometry in the periodic block
-    //m_odometry.update(m_gyro.getRotation2d(), m_leftEncoder.getDistance(),
-    //                  m_rightEncoder.getDistance());
+    m_odometry.update( m_gyro.getRotation2d(), Units.feetToMeters( leftMaster.getSelectedSensorPosition() / 12 ),
+      Units.feetToMeters( rightMaster.getSelectedSensorPosition() / 12 ) );
+
+    // Puts data that we may need into the Shuffleboard to view it
     SmartDashboard.putData( "Drivetrain", m_drive );
     SmartDashboard.putData( "Gyro", m_gyro );
     SmartDashboard.putNumber( "Left Encoder", leftMaster.getSelectedSensorPosition() );
@@ -98,7 +100,23 @@ public class Drivetrain extends SubsystemBase {
     SmartDashboard.putNumber( "Right Velocity", rightMaster.getSelectedSensorVelocity() * 10 );
 
     if( gear != prevGear ){
-      
+      /*
+        INLINE IF STATEMENT
+        Imaginet the inside of the 2 sets below is a variable named v
+        Instead of
+        if( gear == Gear.HighGear ){
+          v = kForward;
+        }else{
+          v = kReverse;
+        }
+        set(v);
+        set(v);
+        We put the if inside the parentheses with an inline if
+        set( boolean ? true value : false value )
+      */
+      gearShiftL.set( gear == Gear.HighGear ? DoubleSolenoid.Value.kForward : DoubleSolenoid.Value.kReverse );
+      gearShiftR.set( gear == Gear.HighGear ? DoubleSolenoid.Value.kForward : DoubleSolenoid.Value.kReverse );
+      prevGear = gear;
     }
   }
 
@@ -170,22 +188,24 @@ public class Drivetrain extends SubsystemBase {
    * @return the average of the two encoder readings
    */
   public double getAverageEncoderDistance() {
-    return ( leftMaster.getSelectedSensorPosition() + rightMaster.getSelectedSensorPosition() ) / 2.0;
+    return ( leftMaster.getSelectedSensorPosition() + rightMaster.getSelectedSensorPosition() ) / 2;
   }
 
+
+  // These are in case another function needs the encoder values, we can give it through the motor
   /**
-   * Gets the left drive encoder.
+   * Gets the left drive master motor
    *
-   * @return the left drive encoder
+   * @return the left drive master motor
    */
   public WPI_TalonFX getLeftMaster() {
     return leftMaster;
   }
 
   /**
-   * Gets the right drive encoder.
+   * Gets the right drive master motor
    *
-   * @return the right drive encoder
+   * @return the right drive master motor
    */
   public WPI_TalonFX getRightMaster() {
     return rightMaster;
@@ -194,14 +214,14 @@ public class Drivetrain extends SubsystemBase {
   /**
    * Sets the max output of the drive.  Useful for scaling the drive to drive more slowly.
    *
-   * @param maxOutput the maximum output to which the drive will be constrained
+   * @param maxOutput the maximum output to which the drive will be constrained, Interval (0, 1]
    */
   public void setMaxOutput( double maxOutput ) {
     m_drive.setMaxOutput(maxOutput);
   }
 
   /**
-   * Zeroes the heading of the robot.
+   * Zeroes the heading of the robot. Wherever we are facing, that is now 0 degrees
    */
   public void zeroHeading() {
     m_gyro.reset();
@@ -226,7 +246,6 @@ public class Drivetrain extends SubsystemBase {
   }
 
   public void setGear( Gear g ){
-	  prevGear = gear;
 	  gear = g;
   }
 
